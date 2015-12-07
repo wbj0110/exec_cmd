6b1253b975d351e8824169b16e9b45dd61c5eb68
soledede <wengbenjue@163.com>
2015-12-07 16:56:37 +0800

    d

--- src/main/java/com/soledede/exec/ExecController.java
+++ src/main/java/com/soledede/exec/ExecController.java
@@ -12,44 +12,44 @@ public class ExecController {
 
     @RequestMapping("/rest")
     public Msg greeting(
-            @RequestParam(value = "cmd") String cmd, @RequestParam(value = "type", defaultValue = "asyn") String type) {
+            @RequestParam(value = "cmd") String cmd,
+            @RequestParam(value = "type", defaultValue = "asyn") String type,
+            @RequestParam(value = "name", defaultValue= "") String name) {
         try {
             if (cmd == null || cmd.trim().equalsIgnoreCase("")) return new Msg("cmd can't be null or ''", null, 1);
             String cmdString = "sh /data/shell/incIndex.sh";
-            if (cmd.trim().equalsIgnoreCase("exec")) {
-                if (type.trim().equalsIgnoreCase("syn")) {
-                    if (ScalaProcess.execStatus() == -1)
-                        return ScalaProcess.exec(cmdString);
-                    else return new Msg("正在处理中", null, 1);
+            if (!cmd.trim().equalsIgnoreCase("status")) {
+                if (cmd.trim().equalsIgnoreCase("mkdir")) {
+                    if (name != null && !name.trim().equalsIgnoreCase("")) {
+                        return ScalaProcess.execMkdir(name.trim());
+                    }
                 } else {
-                    if (ScalaProcess.execStatus() == -1) {
-                       // ScalaProcess.execAsyn(cmdString);
+                    if (type.trim().equalsIgnoreCase("syn")) {
+                        if (ScalaProcess.execStatus() == -1)
+                            return ScalaProcess.exec(cmdString);
+                        else return new Msg("正在处理中", null, 1);
+                    } else {
+                        // ScalaProcess.execAsyn(cmdString);
                         try {
-                            ScalaProcess.eventQueue().put(1);
+                            ScalaProcess.eventQueue().put(cmd.trim());
                         } catch (InterruptedException e) {
                             return new Msg("正在处理...，请勿频繁提交", null, 1);
                         }
                         return new Msg("该请求为异步请求，请通过参数cmd=status来查看", null, 0);
-                    } else {
-                        return new Msg("正在处理中", null, 1);
                     }
                 }
             } else if (cmd.trim().equalsIgnoreCase("status")) {
-                if (ScalaProcess.execStatus() == -1) {
-                    if (!ScalaProcess.stdout().equalsIgnoreCase("") || !ScalaProcess.stderr().equalsIgnoreCase("")) {
-                        Msg msg = new Msg(ScalaProcess.stdout(), ScalaProcess.stderr(),ScalaProcess.execStatus() );
-                        return msg;
-                    } else {
-                        return new Msg("请输入命令exec开始执行", null, 0);
-                    }
+                if (!ScalaProcess.stdout().toString().equalsIgnoreCase("") || !ScalaProcess.stderr().toString().equalsIgnoreCase("")) {
+                    Msg msg = new Msg(ScalaProcess.stdout().toString(), ScalaProcess.stderr().toString(), ScalaProcess.execStatus());
+                    return msg;
                 } else {
-                    return new Msg("正在处理中...,请稍候查看", null, 0);
+                    return new Msg("请输入命令开始执行", null, 0);
                 }
-            } else {
-                return new Msg("forbidden!,请输入正确的参数", null, 1);
-            }
+
+            } else return new Msg("请输正确参数", null, 0);
         } catch (Exception e) {
             return new Msg(e.getMessage(), null, 1);
         }
+        return new Msg("null", null, -1);
     }
 }
--- src/main/java/com/soledede/exec/ScalaProcess.scala
+++ src/main/java/com/soledede/exec/ScalaProcess.scala
@@ -5,6 +5,7 @@ import java.util.concurrent.LinkedBlockingQueue
 
 import com.soledede.exec.entity.{Msg, Progress}
 
+import scala.collection.mutable
 import scala.collection.mutable.StringBuilder
 import sys.process._
 
@@ -12,43 +13,65 @@ import sys.process._
   * Created by soledede on 2015/11/26.
   */
 object ScalaProcess {
-  val eventQueue = new LinkedBlockingQueue[Int](1)
-  val cmd = "sh /data/shell/incIndex.sh"
+  val eventQueue = new LinkedBlockingQueue[String](10000)
 
   @volatile var execStatus: Int = -1
-  @volatile var stdout = new String
-  @volatile var stderr = new String
+  @volatile var stdout = new scala.StringBuilder()
+  @volatile var stderr = new scala.StringBuilder()
   val lock = new Object
 
   def exec(cmd: String): Msg = {
-    stdout = ""
-    stderr = ""
-    execStatus = cmd ! ProcessLogger(stdout = _, stderr = _)
+    stdout = new scala.StringBuilder()
+    stderr = new scala.StringBuilder()
+    execStatus = cmd ! ProcessLogger(stdout append _, stderr append _)
     val msg = new Msg(stdout.toString(), stderr.toString(), execStatus)
-    stdout = ""
-    stderr = ""
     execStatus = -1
     msg
   }
 
-  def execAsyn(cmd: String) = {
+  def execMkdir(dir: String): Msg = {
+    stdout = new scala.StringBuilder()
+    stderr = new scala.StringBuilder()
+    //mkdir "mkdir -p /data/ftp/solr/searchcloud/xml/zhong/"!
+    s"mkdir -p /data/ftp/solr/searchcloud/xml/${dir}/" ! ProcessLogger(stdout append _, stderr append _)
+    val msg = new Msg(stdout.toString(), stderr.toString(), execStatus)
+    execStatus = -1
+    msg
+  }
 
+  def execAsyn(newDir: String) = {
+    stdout = new scala.StringBuilder()
+    stderr = new scala.StringBuilder()
     lock.synchronized {
-      stdout = ""
-      stderr = ""
-      execStatus = cmd ! ProcessLogger(stdout = _, stderr = _)
+      //println(newDir)
+      //"echo -e  /usr/local/solr/solr/bin/post -p 10032 -c searchcloud /data/ftp/solr/searchcloud/xml/zhong/*.XML" #>> new java.io.File("/data/ftp/solr/searchcloud/xml/zhong_tmp.sh")!
+      s"echo -e  /usr/local/solr/solr/bin/post -p 10032 -c searchcloud /data/ftp/solr/searchcloud/xml/${newDir}/*.XML" #>> new java.io.File(s"/data/ftp/solr/searchcloud/xml/${newDir}_tmp.sh") ! ProcessLogger(stdout append _, stderr append _)
+      //"echo -e mv /data/ftp/solr/searchcloud/xml/zhong/*.XML /data/ftp/solr/searchcloud/backup/" #>> new java.io.File("/data/ftp/solr/searchcloud/xml/zhong_tmp.sh")!
+      s"echo -e mv /data/ftp/solr/searchcloud/xml/${newDir}/*.XML /data/ftp/solr/searchcloud/backup/" #>> new java.io.File(s"/data/ftp/solr/searchcloud/xml/${newDir}_tmp.sh") ! ProcessLogger(stdout append _, stderr append _)
+      //"echo -e rm -rf /data/ftp/solr/searchcloud/xml/zhong/" #>> new java.io.File("/data/ftp/solr/searchcloud/xml/zhong_tmp.txt")!
+      s"echo -e rm -rf /data/ftp/solr/searchcloud/xml/${newDir}" #>> new java.io.File(s"/data/ftp/solr/searchcloud/xml/${newDir}_tmp.sh") ! ProcessLogger(stdout append _, stderr append _)
+      s"echo -e rm -rf /data/ftp/solr/searchcloud/xml/${newDir}_tmp.sh" #>> new java.io.File(s"/data/ftp/solr/searchcloud/xml/${newDir}_tmp.sh") ! ProcessLogger(stdout append _, stderr append _)
+      // "chmod 777 /data/ftp/solr/searchcloud/xml/zhong_tmp.txt"!
+      s"chmod 777 /data/ftp/solr/searchcloud/xml/${newDir}_tmp.sh" ! ProcessLogger(stdout append _, stderr append _)
+      //"sh /data/ftp/solr/searchcloud/xml/zhong_tmp.txt"!
+      s"sh /data/ftp/solr/searchcloud/xml/${newDir}_tmp.sh" ! ProcessLogger(stdout append _, stderr append _)
       execStatus = -1
     }
   }
 
   private var thread = new Thread("exec Thread") {
     setDaemon(true)
+
     override def run() {
-      while(true){
-        eventQueue.take()
-        execAsyn(cmd)
+      while (true) {
+        val dirName = eventQueue.take()
+        execAsyn(dirName)
       }
     }
   }
   thread.start()
+
+  def main(args: Array[String]) {
+    execAsyn("zhong")
+  }
 }
